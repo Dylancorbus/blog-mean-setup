f4c752000f88bf334d34a46f17d976acd2a508c0
Dylan C <dylancorbus@outlook.com>
2020-09-17 00:10:14 -0700

    Update index.js

--- dbs/index.js
+++ dbs/index.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-const BLOG_URI = "mongodb://root:Bo\@rdlife@172.26.0.229:27017/Blog"
+const BLOG_URI = ""
 
 function connect(url) {
   return MongoClient.connect(url).then(client => client.db())
